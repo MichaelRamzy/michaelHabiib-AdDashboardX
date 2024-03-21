d7b86a5588d3c727006f0693c0a8c7a291273787
Michael Habib <ramzymika@gmail.com>
2024-03-21 22:41:20 +0200

    Added Dependencies (Angular material, firebase, Bootstrap)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StoreModule } from '@ngrx/store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule} from '@angular/fire/compat'
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+const firebaseConfig = {
+  apiKey: "AIzaSyB5-S577JQ5k1VZ3Udh_2kMXfJSRxeOF74",
+  authDomain: "addashboardx.firebaseapp.com",
+  projectId: "addashboardx",
+  storageBucket: "addashboardx.appspot.com",
+  messagingSenderId: "314091997496",
+  appId: "1:314091997496:web:38290392ca2bbb218259cd",
+  measurementId: "G-Q6QDP5YF7C"
+};
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
+    StoreModule.forRoot({}, {}),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
